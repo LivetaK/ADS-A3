d87d6c1eec62e2020328d13ad85938216bd157b0
LivetaK <159531709+LivetaK@users.noreply.github.com>
2024-05-28 11:48:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <vector>
 
-void rikiavimas(std::vector<int>& nesurikiuotas);
+using namespace std;
+
+void rikiavimas(vector<int>& nesurikiuotas);
 
 int main() {
-    std::vector<int> nesurikiuotas = { 1,3,5,4,6,2,8,7,9};
+    vector<int> nesurikiuotas = { 1,3,5,4,6,2,8,7,9};
     rikiavimas(nesurikiuotas);
-    std::cout << "surikiuotas masyvas: ";
+    cout << "surikiuotas masyvas: ";
     for (const int& num : nesurikiuotas) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
     return 0;
 }
 
-void rikiavimas(std::vector<int>& nesurikiuotas) {
+void rikiavimas(vector<int>& nesurikiuotas) {
     for (int i = 1; i < nesurikiuotas.size(); i++) {
         int laikinas = nesurikiuotas[i];
         int j = i - 1;
